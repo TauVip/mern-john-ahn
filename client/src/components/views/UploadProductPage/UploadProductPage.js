15c951a065f95c3f15fbf53a9bf22443e5602c74
TauVip <Abylai1393@gmail.com>
2021-03-08 22:16:24 +0600

    first commit

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function UploadProductPage() {
+  return <div>666</div>
+}
+
+export default UploadProductPage
---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://AB:87783937404argyn@cluster0.8q5zj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
